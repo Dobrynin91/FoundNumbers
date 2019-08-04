67fe1ba45b805fd75f0cc44f27fc563eece0c27a
Philip Dobrynin <dobrynin.ph@yandex.ru>
2019-08-04 20:15:46 +0400

    Create the project

---
+++ Documents/NetBeansProjects/FindNumberProject/src/ru/example/main/ConnectionClass.java
@@ -0,0 +1,5 @@
+package ru.example.main;
+
+public class ConnectionClass {
+    
+}
---
+++ Documents/NetBeansProjects/FindNumberProject/src/ru/example/main/FindNumberProject.java
@@ -0,0 +1,9 @@
+package ru.example.main;
+
+public class FindNumberProject {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
